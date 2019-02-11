0136efea869e740d3cf9f160c7edd3ac3cfc8bba
Leon Weber <leonweber@posteo.de>
2019-02-11 13:52:19 +0100

    add start_server.sh

---
+++ start_server.sh
@@ -0,0 +1,4 @@
+docker pull huner/huner;
+docker kill "$1" 2> /dev/null > /dev/null;
+docker rm "$1" 2> /dev/null > /dev/null;
+docker run -t -i --name "$1" --volume ${PWD}:/usr/huner huner/huner /bin/bash -c "cd /usr/huner; python tagger.py $1"
