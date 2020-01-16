1628fca9049927d4cdf358fabfaa3cc662b268e5
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2020-01-16 13:13:46 +0100

    Convert script for CRAFT corpus

---
+++ ner_scripts/convert_craft.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Craft corpus can be obtained from http://sourceforge.net/projects/bionlp-corpora/files/CRAFT/v2.0/craft-2.0.tar.gz/download
+
+CRAFT_DIR=$1
+
+python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR chebi craft_chemicals_tmp.conll
+awk '{print $2"\t"$3"\t"$4}' craft_chemicals_tmp.conll > craft_chemicals.conll
+rm craft_chemicals_tmp.conll
+
+python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR entrezgene,pr craft_genes_tmp.conll
+awk '{print $2"\t"$3"\t"$4}' craft_genes_tmp.conll > craft_genes.conll
+rm craft_genes_tmp.conll
+
+python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR ncbitaxon craft_species_tmp.conll
+awk '{print $2"\t"$3"\t"$4}' craft_species_tmp.conll > craft_species.conll
+rm craft_species_tmp.conll
