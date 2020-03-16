d5687c69299a2e4941c5680711815a27b2c6f8fe
yetinam <munchmej@informatik.hu-berlin.de>
2020-03-16 16:54:56 +0100

    Fix paths in convert_craft.sh

--- ner_scripts/convert_craft.sh
+++ ner_scripts/convert_craft.sh
@@ -4,14 +4,14 @@
 
 CRAFT_DIR=$1
 
-python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR chebi craft_chemicals_tmp.conll
+python scripts/craft_to_conll.py $CRAFT_DIR chebi craft_chemicals_tmp.conll
 awk '{print (NF ? $2"\t"$3"\t"$4 : $0)}' craft_chemicals_tmp.conll > craft_chemicals.conll
 rm craft_chemicals_tmp.conll
 
-python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR entrezgene,pr craft_genes_tmp.conll
+python scripts/craft_to_conll.py $CRAFT_DIR entrezgene,pr craft_genes_tmp.conll
 awk '{print (NF ? $2"\t"$3"\t"$4 : $0)}' craft_genes_tmp.conll > craft_genes.conll
 rm craft_genes_tmp.conll
 
-python ~/code/huner/ner_scripts/scripts/craft_to_conll.py $CRAFT_DIR ncbitaxon craft_species_tmp.conll
+python scripts/craft_to_conll.py $CRAFT_DIR ncbitaxon craft_species_tmp.conll
 awk '{print (NF ? $2"\t"$3"\t"$4 : $0)}' craft_species_tmp.conll > craft_species.conll
 rm craft_species_tmp.conll
